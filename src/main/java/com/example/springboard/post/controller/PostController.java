40035ddd9da9cabdd5b20e013b5b68171eabca83
Song Minjin <m_6595@naver.com>
2023-05-15 14:27:45 +0900

    Interceptor 제대로 적용

--- src/main/java/com/example/springboard/post/controller/PostController.java
+++ src/main/java/com/example/springboard/post/controller/PostController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.example.springboard.comment.service.CommentService;
 import com.example.springboard.comment.vo.CommentVO;
@@ -54,34 +55,18 @@ public class PostController {
 	}
 
 	@GetMapping("board")
-	public String showBoard(HttpSession session, @ModelAttribute("posts") List<PostVO> posts) {
-		
-		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
-		if (loginMember == null) {
-			throw new BusinessException(ErrorCode.INVALID_LOGIN);
-		}
-		
+	public String showBoard(@ModelAttribute("posts") List<PostVO> posts) {
 		posts.addAll(postService.loadPosts());
 		return "board";
 	}
 	
 	@GetMapping("post/new")
-	public String goToNewPostPage(HttpSession session) {
-		
-		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
-		if (loginMember == null) {
-			throw new BusinessException(ErrorCode.INVALID_LOGIN);
-		}
+	public String goToNewPostPage() {
 		return "newpost";
 	}
 	
 	@PostMapping("post/new")
-	public String createPost(@ModelAttribute("post") PostVO requestVO, HttpSession session) {
-		
-		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
-		if (loginMember == null) {
-			throw new BusinessException(ErrorCode.INVALID_LOGIN);
-		}
+	public String createPost(@ModelAttribute("post") PostVO requestVO, @SessionAttribute MemberVO loginMember) {
 		requestVO.setMemberId(loginMember.getId());
 		requestVO.setMemberNickname(loginMember.getNickname());
 	
@@ -91,13 +76,8 @@ public class PostController {
 	}
 	
 	@GetMapping("post/{postId}")
-	public String goToPost(@PathVariable("postId") int postId, Model model, HttpSession session) {
-		log.info("post/" + postId + "호출 완료");	
-		
-		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
-		if (loginMember == null) {
-			throw new BusinessException(ErrorCode.INVALID_LOGIN);
-		}
+	public String goToPost(@PathVariable("postId") int postId, Model model) {
+		log.info("post/" + postId + "호출 완료");
 		
 		PostVO postVO = postService.showPost(postId);
 		List<CommentVO> comments = commentService.loadPostComments(postId);
@@ -109,11 +89,7 @@ public class PostController {
 	}
 		
 	@GetMapping("post/{postId}/edit")
-	public String goToEditPostPage(@PathVariable("postId") int postId, Model model, HttpSession session) {
-	    MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
-	    if (loginMember == null) {
-	        throw new BusinessException(ErrorCode.INVALID_LOGIN);
-	    }
+	public String goToEditPostPage(@PathVariable("postId") int postId, Model model) {
 	    PostVO postVO = postService.showPost(postId);
 	    model.addAttribute("post", postVO);
 	    return "editpost";
@@ -121,26 +97,15 @@ public class PostController {
 	
 	@PutMapping(value = "post/{postId}/edit")
 	@ResponseBody
-	public ResponseEntity<PostVO> editPost(@PathVariable("postId") int postId, @ModelAttribute("post") PostVO postVO, HttpSession session) {
-		log.info("수정하려고 들어온 데이터=" + postVO);
-		
-		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
-		log.warn(loginMember);
-		if (loginMember == null) {
-			throw new BusinessException(ErrorCode.INVALID_LOGIN);
-		}
-		
+	public ResponseEntity<PostVO> editPost(@PathVariable("postId") int postId, @ModelAttribute("post") PostVO postVO, HttpSession session) {		
 		postVO.setId(postId);
 		PostVO result = postService.updatePost(postVO);
-		session.setAttribute("post", result); // 수정된 결과 객체를 세션에 저장
-		
-		log.info("수정 controller 메서드 수행 완료");
-		
+		session.setAttribute("post", result); // 수정된 결과 객체를 세션에 저장		
 		return new ResponseEntity<PostVO>(result, HttpStatus.OK);
 	}
 
 	@DeleteMapping(value = "post/{postId}")
-	public String deletePost(@PathVariable("postId") int postId, HttpSession session) {
+	public String deletePost(@PathVariable("postId") int postId) {
 		
 		postService.deletePost(postId);
 				
